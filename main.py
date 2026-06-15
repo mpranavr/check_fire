77043e3fb6ff21cd795494c866d48516382e555b
mpranavr <mpranavreddy39@gmail.com>
2026-06-15 15:22:52 +0530

    interval_change

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 # Base interval in minutes
-INTERVAL_TIME_MIN = int(os.getenv("INTERVAL_TIME_MIN", "15"))
+INTERVAL_TIME_MIN = int(os.getenv("INTERVAL_TIME_MIN", "7"))
 
 # Random delay added after each check (seconds)
 RANDOM_OFFSET_MIN = 0
-RANDOM_OFFSET_MAX = 20
+RANDOM_OFFSET_MAX = 180
 
 HEADERS = {
     "Accept": "application/json",
